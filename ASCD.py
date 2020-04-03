3b8cf23938b8e01890d2d3fbde79d82eeb381d20
Samuel Gustin <gustin.daniel1@gmail.com>
2020-04-04 00:55:18 +0300

    Derivative
    pre computed gradient in 3 hours on google colab notebook
    made gradient serializable into json
    couldn't load json as size > 100mb

--- ASCD.py
+++ ASCD.py
@@ -1,11 +1,15 @@
-from sympy import *
+from sympy import symbols, Matrix
 import numpy as np
-import ast
 import json
+import time
 
+start = time.time()
 with open('data.json', 'r') as read_file:
     data = json.load(read_file)
 
-x = symbols(data['symbols'])
-f = (Matrix(data['y']) - Matrix(data['X']) * Matrix(x)).norm() ** 2
+theta_curr, theta_prev = symbols('t_curr t_prev')
+theta_fun = (1 - theta_curr) * theta_prev ** 2 - theta_curr ** 2
 
+
+end = time.time()
+print(end - start)
--- generate_data.py
+++ generate_data.py
@@ -1,6 +1,6 @@
 import json
 import numpy as np
-
+from sympy import symbols, Matrix
 
 jon = {
     'n': 200,
@@ -28,6 +28,17 @@ B_star = np.random.normal(0, 1, jon['p'])
 y_mean = X @ B_star
 y = np.random.normal(y_mean, 1)
 jon['y'] = y.tolist()
+
+# compute gradient
+x = symbols(jon['symbols'])
+f = (Matrix(jon['y']) - Matrix(jon['X']) * Matrix(x)).norm() ** 2
+
+f_gradient = []
+for i in range(jon['p']):
+    f_gradient.append(f.diff(x[i]))
+
+# make list of Add objects serializable
+jon['f_gradient'] = [str(f_gradient[i]) for i in range(len(f_gradient))]
 
 with open('data.json', 'w') as write_file:
     json.dump(jon, write_file)
